d883b5009f7f8d9ae8c03c167434ac95b653422b
Grayson Wyker <gwyker@yahoo.com>
2019-03-06 00:49:24 -0500

    upadte3

--- fireflies.js
+++ fireflies.js
@@ -13,28 +13,15 @@ function setup() {
 
 function wave() {
   fill(80);
-  // We are going to draw a polygon out of the wave points
   beginShape();
 
-  let xoff = 0; // Option #1: 2D Noise
-  // let xoff = yoff; // Option #2: 1D Noise
+  let xoff = 0;
 
-  // Iterate over horizontal pixels
   for (let x = 0; x <= width; x += 10) {
-    // Calculate a y value according to noise, map to
-
-    // Option #1: 2D Noise
     let y = map(noise(xoff, yoff), 0, 1, wave_crest, wave_trough);
-
-    // Option #2: 1D Noise
-    // let y = map(noise(xoff), 0, 1, 200,300);
-
-    // Set the vertex
     vertex(x, y);
-    // Increment x dimension for noise
     xoff += 0.05;
   }
-  // increment y dimension for noise
   yoff += 0.01;
   vertex(width, height);
   vertex(0, height);
